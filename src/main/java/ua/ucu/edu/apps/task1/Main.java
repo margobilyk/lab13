a64600299f32219ba43138a860a3a6a905943aaf
margobilyk <m.bilyk.pn@ucu.edu.ua>
2024-12-09 15:33:13 +0200

    ?

--- src/main/java/ua/ucu/edu/apps/task1/Main.java
+++ src/main/java/ua/ucu/edu/apps/task1/Main.java
@@ -7,20 +7,26 @@ public class Main {
         AuthMethod authMethod = AuthMethod.FACEBOOK;
         User user = null;
 
-        switch(authMethod) {
+        switch (authMethod) {
             case FACEBOOK:
-                FacebookUser facebookUser
-                = new FacebookUser("test@email", "Ukraine", LocalDateTime.now().toString());
+                FacebookUser facebookUser =
+                    new FacebookUser("test@email", "Ukraine", LocalDateTime.now().toString());
                 user = new FacebookUserAdapter(facebookUser);
                 break;
-    
+
             case TWITTER:
-                TwitterUser twitterUser
-                = new TwitterUser("test@email","USA", LocalDateTime.now().minusDays(0).toString());
+                TwitterUser twitterUser =
+                    new TwitterUser("test@email", "USA", LocalDateTime.now().minusDays(0).toString());
+                user = new TwitterUserAdapter(twitterUser);
                 break;
 
             default:
                 break;
+        }
+
+        if (user != null) {
+            MessageSender sender = new MessageSender();
+            sender.send(user, "Hello, please give me a good grade!!");
         }
     }
 }
